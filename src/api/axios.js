c7cf37e7e9218d922e974924be305cf91b34b5e2
Monali Gupta <monaligupta06@gmail.com>
2026-04-24 13:48:13 +0530

    final

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:8080/api", 
+    baseURL:"https://ecommerce-be-ig9u.onrender.com", 
     withCredentials: true
 });
 
